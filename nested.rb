9d3900e9bb6ad436d1f8636c9df15bc3cea59d10
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 22:30:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-yuki
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby", 
+  :languages => ["LISP", "C"]
+}
+
+programmer_hash
 end
 
 def changing_alan
